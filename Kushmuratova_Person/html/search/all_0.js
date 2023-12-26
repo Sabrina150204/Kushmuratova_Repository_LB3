1f49e3b779f365a919fe71affa2dbec2066627e7
Sabrina <skushmuratova@bk.ru>
2023-12-27 01:02:24 +0300

    First commit

---
+++ Kushmuratova_Person/html/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['phone_0',['Phone',['../class_phone.html',1,'Phone'],['../class_phone.html#aa8a475277cd45c20febdf574a08de944',1,'Phone::Phone()']]]
+];
---
+++ Kushmuratova_Person/html/search/all_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['showabonentswithexceededlocalcalltime_0',['showAbonentsWithExceededLocalCallTime',['../class_phone.html#a253182aa585c2ae03f2b0ed1452fc585',1,'Phone']]],
+  ['showabonentswithlongdistancecalls_1',['showAbonentsWithLongDistanceCalls',['../class_phone.html#ae3d20f8351af0dc167185ad5ee64e743',1,'Phone']]]
+];
